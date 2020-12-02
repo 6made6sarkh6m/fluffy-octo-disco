33c7962f028144e3dd08484c6f75fe124c271462
Amadeus <amadeusarkhammorningstar@gmail.com>
2020-12-02 18:30:45 +0600

    refactored html menu cards by using classes

--- js/script.js
+++ js/script.js
@@ -130,5 +130,66 @@ window.addEventListener('DOMContentLoaded',()=>{
     }
     window.addEventListener('scroll',showModalByScroll)
 
+    //Cards
+
+    class Card {
+        constructor(src,alt,title, description, price, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.transfer  = 399;
+            this.parentSelector = document.querySelector(parentSelector);
+            this.changeToUAH();
+        }
+        changeToUAH(){
+            this.price = this.price*this.transfer;
+
+        }
+        render(){
+            const element = document.createElement('div');
+            element.innerHTML = ` <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.description}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>`;
+        this.parentSelector.append(element);
+        }
+    }
+    // const div = new Card();
+    // div.render();
+
+    new Card(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню “Премиум”",
+        "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        550,
+        ".menu .container"
+    ).render();
+
+    new Card(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню “Премиум”",
+        "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        550,
+        ".menu .container"
+    ).render();
+    
+    new Card(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню “Премиум”",
+        "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        550,
+        ".menu .container"
+    ).render();
     
 });
